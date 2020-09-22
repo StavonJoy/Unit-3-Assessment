26ac3dbea58a98ff9b219c8edfb5b288229b954c
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-09-22 14:36:54 -0400

    Updates

---
+++ precache-manifest.42f222e02f97b76ea24f5f92af1d1cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b983a60e9acebdfe1bd298459c0220",
+    "url": "/StavonJoy/Unit-3-Assessment.git/index.html"
+  },
+  {
+    "revision": "9ab941980ce46589b4fb",
+    "url": "/StavonJoy/Unit-3-Assessment.git/static/css/main.b9be8532.chunk.css"
+  },
+  {
+    "revision": "71832ccc29cf1bdc4094",
+    "url": "/StavonJoy/Unit-3-Assessment.git/static/js/2.ad46cd44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StavonJoy/Unit-3-Assessment.git/static/js/2.ad46cd44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab941980ce46589b4fb",
+    "url": "/StavonJoy/Unit-3-Assessment.git/static/js/main.6087ed06.chunk.js"
+  },
+  {
+    "revision": "71d3b07259210533ea4a",
+    "url": "/StavonJoy/Unit-3-Assessment.git/static/js/runtime-main.af18b4e0.js"
+  }
+]);
